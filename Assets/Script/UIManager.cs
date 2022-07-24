1514095b07bb0c93fe756c25b1c1a7a3d84c357c
DESKTOP-IAEPM7R\vantan <04022005rikuriku@gmail.com>
2022-07-24 12:09:50 +0900

    UIManagerを追加

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,16 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
+    const string SCENE_NAME = "GameScene";
+    public void Retry() => SceneManager.LoadScene(SCENE_NAME);
+    
 }
